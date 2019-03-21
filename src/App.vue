bca9561e33475395463d93af7f71cc2e266e72cb
songqian <iamsongqian>
2019-03-21 11:44:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Myex />
+    <Hello />
+  </div>
+</template>
+<script>
+import Myex from './components/Myex'
+import Hello from './components/HelloWorld'
+export default {
+     data(){
+         return{
+         }
+     },
+     components:{
+       Myex ,
+       Hello
+     }
+ }
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
