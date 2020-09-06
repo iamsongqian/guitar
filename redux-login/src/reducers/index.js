337318733c11d4964ee94a05fcd2e297484491e4
songqian <iamsongqian@qq.com>
2020-09-06 20:40:41 +0800

    fix: new project

--- redux-login/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import auth from './auth'
-import logInfo from './logInfo'
-const rootReduces = combineReducers({
-  auth,
-  logInfo
-})
-
-export default rootReduces
